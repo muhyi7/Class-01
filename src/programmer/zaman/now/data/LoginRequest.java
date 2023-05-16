184d26eb114de34544e4433b8f82228cc0b7f6da
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-05-17 05:24:34 +0700

    hampir selesai

---
+++ src/programmer/zaman/now/data/LoginRequest.java
@@ -0,0 +1,18 @@
+package programmer.zaman.now.data;
+
+public record LoginRequest(String username,String password) {
+
+    public LoginRequest{
+        System.out.println("Membuat object LoginRequest");
+    }
+
+    public LoginRequest(String username){
+        this(username,"");
+    }
+
+    public LoginRequest(){
+        this("","");
+    }
+
+
+}
