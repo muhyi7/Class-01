8ef8e9058c0ba85bd16ed546eb0d0353d091c167
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-05-24 17:14:43 +0700

    Materi Exception

---
+++ src/programmer/zaman/now/application/ValidationApp.java
@@ -0,0 +1,23 @@
+package programmer.zaman.now.application;
+
+import programmer.zaman.now.data.LoginRequest;
+import programmer.zaman.now.error.ValidationException;
+import programmer.zaman.now.util.ValidationUtil;
+
+public class ValidationApp {
+    public static void main(String[] args) {
+        LoginRequest loginRequest = new LoginRequest("eko","rahasia");
+        try {
+            ValidationUtil.validate(loginRequest);
+            System.out.println("Data Valid");
+        }catch (ValidationException | NullPointerException exception){
+            System.out.println("Data tidak valid : " + exception.getMessage());
+        }finally {
+            System.out.println("Selalu di eksekusi");
+        }
+
+        LoginRequest loginRequest2 = new LoginRequest(null,null);
+        ValidationUtil.validateRuntimeException(loginRequest2);
+        System.out.println("sukses");
+    }
+}
---
+++ src/programmer/zaman/now/util/ValidationUtil.java
@@ -0,0 +1,32 @@
+package programmer.zaman.now.util;
+
+import programmer.zaman.now.data.LoginRequest;
+import programmer.zaman.now.error.BlankException;
+import programmer.zaman.now.error.ValidationException;
+
+public class ValidationUtil {
+
+    public static void validate(LoginRequest loginRequest)throws ValidationException, NullPointerException{
+        if(loginRequest.username() == null){
+            throw new NullPointerException("Username is null");
+        }else if(loginRequest.username().isBlank()){
+            throw new ValidationException("Username is null");
+        }else if(loginRequest.password() == null){
+            throw new NullPointerException("Password is null");
+        }else if(loginRequest.password().isBlank()){
+            throw new ValidationException("Password is null");
+        }
+    }
+    public static void validateRuntimeException(LoginRequest loginRequest) {
+        if(loginRequest.username() == null){
+            throw new NullPointerException("Username is null");
+        }else if(loginRequest.username().isBlank()){
+            throw new BlankException("Username is null");
+        }else if(loginRequest.password() == null){
+            throw new NullPointerException("Password is null");
+        }else if(loginRequest.password().isBlank()){
+            throw new BlankException("Password is null");
+        }
+    }
+
+}
